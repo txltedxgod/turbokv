4c7113788441060ab7278aa4e6b30ac8a9f07bd7
txltedxgod <seidoutakidzava@gmail.com>
2026-08-21 11:24:26 +0400

    docs: add quickstart example script

---
+++ examples/client_demo.cpp
@@ -0,0 +1,10 @@
+// TurboKV - Minimal TCP Client Demo
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cout << "TurboKV Client Demo" << std::endl;
+    std::cout << "Usage: SET my_key my_value [ttl_ms]" << std::endl;
+    std::cout << "       GET my_key" << std::endl;
+    return 0;
+}
